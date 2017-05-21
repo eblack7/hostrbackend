b688a84cf0fe21c2b9379f5adbec742cd4631be0
Sai Kiran <kirandasika30@gmail.com>
2017-05-21 09:23:11 -0500

    PyCharm commit

--- users/views.py
+++ users/views.py
@@ -3,8 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-import firebase_admin
-from firebase_admin import credentials
+import json
+from .models import User
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
+
 # Create your views here.
 def index(request):
-	return HttpResponse("Welcome to users")
+    return HttpResponse(json.dumps({'response': 'Welcome to users app'}),
+                        content_type="application/json")
+
+
+@csrf_exempt
+def all(request):
+    users = User.objects.all()
+    return HttpResponse(serializers.serialize("json", users),
+                        content_type="application/json")
