2c3fe26fd9b8bb3f75cfb5b5d6b60f5eae016665
Sai Kiran <kirandasika30@gmail.com>
2017-12-05 08:18:26 -0600

    linting for deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Default deployment script for Backend
 # Deoploy to gcp
+
+# Activate virtual env
 source env/bin/activate
+# Installing all vendor packages to lib folder
 pip install -r requirements.txt -t lib/
-pip install -r requirements.txt
+# Installing all dependencies to lib folder
+pip install -r requirements.txt -t lib/
+# Upload app to gcloud
 gcloud app deploy
+# Removing lib folder
 rm -rf lib/
+#Deployment confirmation
 echo "Deployed..."
