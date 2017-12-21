5408a97c830037ea3a37ca52f74a76ad9e5c72f9
Sai Kiran <kirandasika30@gmail.com>
2017-12-21 12:06:30 -0600

    revert to django admin

--- system/urls.py
+++ system/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view()),
     url(r'^login/', views.LoginView.as_view(), name="systemz_login"),
+    url(r'dashboard/', views.DashboardView.as_view(), name="systemz_dashboard"),
 ]
--- system/views.py
+++ system/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic import TemplateView, View
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.generic import View, TemplateView
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -10,6 +10,14 @@ class IndexView(TemplateView):
     template_name = "system/index.html"
 
 class LoginView(View):
-    """ Sameple login view"""
+    """ Logs a admin into systemz. """
+
     def post(self, request):
-        return HttpResponse("lol")
+        """ Checks username and password and logs in user. """
+        user_context = {'username': request.POST['username'], 'id': 1}
+        return redirect("systemz_dashboard")
+
+
+class DashboardView(TemplateView):
+    """ Main Dashboard View """
+    template_name = "system/dashboard.html"
