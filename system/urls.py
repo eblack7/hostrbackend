247e92fc3a55e146adf9bcb8fa8372a99542c416
Sai Kiran <kirandasika30@gmail.com>
2017-12-22 13:41:32 -0600

    systemz views and templating

--- system/urls.py
+++ system/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view()),
+    url(r'^$', views.IndexView.as_view(), name="systemz_index"),
     url(r'^login/', views.LoginView.as_view(), name="systemz_login"),
-    url(r'dashboard/', views.DashboardView.as_view(), name="systemz_dashboard"),
+    url(r'^logout/', views.LogoutView.as_view(), name="systemz_logout"),
+    url(r'^event/(?P<event_id>[0-9]+)', views.EventDetailView.as_view(), name="systemz_event_detail"),
+    url(r'^(?P<uid>[0-9]+)/dashboard$', views.DashboardView.as_view(), name="systemz_dashboard")
 ]
--- system/views.py
+++ system/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.views.generic import View, TemplateView
+
+from events.models import Event
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -14,10 +19,42 @@ class LoginView(View):
 
     def post(self, request):
         """ Checks username and password and logs in user. """
-        user_context = {'username': request.POST['username'], 'id': 1}
-        return redirect("systemz_dashboard")
+        user = authenticate(request=request, username=request.POST['systemz_username'],
+                            password=request.POST['systemz_password'])
+        if user is not None:
+            login(request, user)
+            return redirect("systemz_dashboard", uid=user.pk)
+        return redirect("systemz_index")
 
+@method_decorator(login_required, name="dispatch")
+class LogoutView(View):
+    """ Logs a user out from systemz. """
 
+    def get(self, request):
+        """ Logout user. """
+        logout(request)
+        return redirect("systemz_index")
+
+@method_decorator(login_required, name="dispatch")
 class DashboardView(TemplateView):
     """ Main Dashboard View """
     template_name = "system/dashboard.html"
+    redirect_field_name = "systemz_index"
+
+    def get(self, request, *args, **kwargs):
+        """ runs when a get request is sent. """
+        cxt = self.get_context_data()
+        cxt['events'] = Event.objects.all()
+        return self.render_to_response(cxt)
+
+@method_decorator(login_required, name="dispatch")
+class EventDetailView(TemplateView):
+    """ shows event details. """
+    template_name = "system/event_detail.html"
+    redirect_field_name = "systemz_index"
+
+    def get(self, request, *args, **kwargs):
+        """ runs when get request is sent """
+        cxt = self.get_context_data()
+        cxt['event_details'] = Event.objects.get(pk=kwargs['event_id'])
+        return self.render_to_response(cxt)
