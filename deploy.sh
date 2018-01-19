4822b339dd9703628d2a9d2d8aceaa91d61843c2
Sai Kiran <kirandasika30@gmail.com>
2018-01-19 10:37:42 -0600

    deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,28 @@ fi
 source env/bin/activate
 
 # Installing all vendor packages to lib folder
-python env/bin/pip install -r requirements-vendor.txt -t lib/
+#python env/bin/pip install -r requirements-vendor.txt -t lib/
 
 # Installing all dependencies to lib folder
-python env/bin/pip install -r requirements.txt -t lib/
+#python env/bin/pip install -r requirements.txt -t lib/
 
 # Collecting all Django default static files
-python manage.py collectstatic
+#python manage.py collectstatic
+
+# Setting up software to run db migrations on app engine
+export SERVER_SOFTWARE='Google App Engine'
+python manage.py migrate
 
 # Upload app to gcloud
 gcloud app deploy
 
 # Removing lib folder
+echo "Removing cached lib folder..."
 rm -rf lib/
+
+# Removing intermediate staticfiles folder
+echo "Removing intermediate staticfiles folder..."
+rm -rf staticfiles/
+
 #Deployment confirmation
-echo "Deployed..."
+echo "Deployed to App Engine."
